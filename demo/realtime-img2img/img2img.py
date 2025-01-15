40cc33dc23ab18e9253ec8e5cf786a46f2e9b56d
timon.grabovac <timon@pareto.si>
2025-01-15 12:33:57 +0100

    change lora model

--- demo/realtime-img2img/img2img.py
+++ demo/realtime-img2img/img2img.py
@@ -95,7 +95,7 @@ class Pipeline:
             use_tiny_vae=True,
             device=device,
             dtype=torch_dtype,
-            t_index_list=[0, 16, 32, 45],
+            t_index_list=[16, 25, 32, 45],
             frame_buffer_size=1,
             width=params.width,
             height=params.height,
