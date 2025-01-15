fc4a2a0439f95828a865fdd62e057fc5b094831f
timon.grabovac <timon@pareto.si>
2025-01-15 09:35:26 +0100

    change lora model

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -49,7 +49,7 @@ class StreamDiffusionWrapper:
         seed: int = 2,
         use_safety_checker: bool = False,
         engine_dir: Optional[Union[str, Path]] = "engines",
-        u2net_model_path: str = "/home/ubuntu/models/u2netp.onnx",  # Path to U²-Net model
+        u2net_model_path: str = "/home/ubuntu/models/u2net_human_seg.onnx",  # Path to U²-Net model
         u2net_threshold: float = 0.5,  # Threshold for mask binarization
     ):
         """
